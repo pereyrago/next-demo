26a0e415a0703e0eb31b8c880487460e6f5b9ab2
pereyrago <pereyrago@gmail.com>
2023-09-20 09:33:54 -0300

    third deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,11 @@ export default async function Home() {
   const users = data?.users;
   return (
     <>
-      <Hero />
+      <Hero
+        title="Invertimos en tu educación"
+        subTitle="Comienza a estudiar Desarrollo Full Stack o Data Science"
+        description="Despega tu carrera en tecnología. Estudia a remoto y de manera intensiva. ¿Y lo mejor? Solo nos pagas cuando consigues un nuevo trabajo."
+      />
       <Container title="Testimonios">
         <>
           {users?.map((user) => (
